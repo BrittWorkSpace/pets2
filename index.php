6ed26cd2709df1183c45dee156dfae20b4180871
Andrew Harrington <aharrington4@mail.greenriver.edu>
2019-04-19 16:07:28 -0400

    Added basic index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->route('GET|POST /', function (){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
